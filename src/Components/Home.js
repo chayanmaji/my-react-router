3d96180a6b033696fb94dba7252b3d2eaad4936c
Chayan Maji <chayanmaji@gmail.com>
2022-02-06 00:59:38 -0500

    sucessfully implemented the changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div>Home page.</div>
+        <>
+            <div>Home page.</div>
+            <button onClick={()=>{ navigate('order-summary', { replace:true }) }}>Place Order</button>
+        </>
     )
 }
 
