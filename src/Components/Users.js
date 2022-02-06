ac24f9703dac0669846cfd98e0ceda9640627dac
Chayan Maji <chayanmaji@gmail.com>
2022-02-06 18:12:42 -0500

    Successfully implemented the search param

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,9 +1,10 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useSearchParams } from 'react-router-dom';
 
 const Users = () => {
-
+    const [searchParams, setSearchParams] = useSearchParams();
     const users = [1,2,3,4];
-
+    console.log('searchParams', searchParams);
+    const showActiveUsers = searchParams.get('filter') === 'active';
     return (
         <>
             <div>
@@ -14,6 +15,12 @@ const Users = () => {
                 }
             </div>
             <Outlet/>
+            <div>
+                <button onClick={()=>{ setSearchParams({ filter:'active' })}}>Active Users</button>
+                <button onClick={()=>{ setSearchParams({})}}>Reset Users</button>
+            </div>
+            {showActiveUsers && <h2>Showing Active Users</h2>}
+            {!showActiveUsers && <h2>Showing All Users</h2>}
         </>
     )
 }
