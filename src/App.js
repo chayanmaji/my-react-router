580effbe081c28fe37e9c8261582bcc20f295227
Chayan Maji <chayanmaji@gmail.com>
2022-02-05 22:44:31 -0500

    Router has been setup successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </>
   );
 }
 
